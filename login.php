ad1c5e86e327d26676a905e2a7224bfa357d427d
rick2909 <rick29998@gmail.com>
2019-03-22 17:26:37 +0100

    add db conection and login

---
+++ login.php
@@ -0,0 +1,41 @@
+<?php
+session_start();
+//lees het config
+require_once 'include/config.inc.php';
+
+//lees post
+$a = $_POST['email'];
+$b = $_POST['password'];
+
+$email = htmlspecialchars(mysqli_real_escape_string($mysqli, $a));
+$password = htmlspecialchars(mysqli_real_escape_string($mysqli, $b));
+
+//make response
+$response = array();
+$response['error'] = TRUE;
+
+//check if filled
+if (strlen($email) > 0 && strlen($password) > 0) {
+	//encrypt
+	$password = sha1($password);
+
+	//query for control
+	$query = "SELECT * FROM `gebruikers` WHERE `email` = '$email' AND `wachtwoord` = '$password'";
+
+	//query work
+	$result = mysqli_query($mysqli, $query);
+
+	//check if login correct
+	if (mysqli_num_rows($result) == 1){
+		$_SESSION['user'] = "admin";
+		$response['error'] = FALSE;
+		$response['message'] = "u bent ingelogd";
+		$response['user'] = $result;
+	} else {
+		$response['message'] = 'verkeerde wachtwoord en of gebruikersnaam';
+	}
+} else{
+	$response['message'] = 'Er is niks ingevoerd';
+}
+echo json_encode($response);
+?>
